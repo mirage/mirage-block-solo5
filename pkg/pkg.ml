fede0286b9eb7f835875410a02efe7bb2ddb9b05
Martin Lucina <martin.lucina@docker.com>
2016-11-22 11:56:29 +0100

    Add LICENSE.md
    Copied from mirage-console-solo5. Fixes #4.

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -5,7 +5,6 @@ open Topkg
 
 let () =
   let change_logs = [ Pkg.std_file "CHANGES" ] in
-  let licenses = [] in
-  Pkg.describe ~change_logs ~licenses
+  Pkg.describe ~change_logs
   "mirage-block-solo5" @@ fun c ->
   Ok [ Pkg.mllib "lib/mirage_block_solo5.mllib"; ]
