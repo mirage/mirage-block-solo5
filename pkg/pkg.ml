69ed369cb867749b5a916d89bd2957c27c0830f5
Clément Pascutto <clement@tarides.com>
2019-02-26 17:05:52 +0100

    Revert "Port to dune"

---
+++ pkg/pkg.ml
@@ -0,0 +1,12 @@
+#!/usr/bin/env ocaml
+#use "topfind"
+#require "topkg"
+open Topkg
+
+let () =
+  let lint_deps_excluding =
+    Some [ "mirage-solo5" ]
+  in
+  let opams = [ Pkg.opam_file "opam" ~lint_deps_excluding ] in
+  Pkg.describe ~opams "mirage-block-solo5" @@ fun c ->
+  Ok [ Pkg.mllib "src/mirage_block_solo5.mllib"; ]
